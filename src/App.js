3b560e96506cccedd0de3ed2dcb888cab72b30e8
James Hall-Treworgy <jameshall-treworgy@Jamess-MBP.lan>
2021-04-24 15:05:30 -0400

    Added Base setup for routes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Root from './Containers/Root/Root';
+import Home from './Containers/Home/Home';
+import Workout from './Containers/Workout/Workout';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Root>
+        <Route path="/" component={Home} />
+        <Route path="/workout/:id" component={Workout} />
+      </Root>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import classes from './Home.module.scss';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Workouts</h2>
+      </div>
+    );
+  }
+
+}
+
+export default Home;
