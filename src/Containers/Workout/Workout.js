164dbdebec059b0aa4673e1635c62ed678a5ab1b
James Hall-Treworgy <jameshall-treworgy@Jamess-MBP.lan>
2021-05-01 19:43:46 -0400

    Add Set and Set Container Component.

--- src/Containers/Workout/Workout.js
+++ src/Containers/Workout/Workout.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import SetContainer from '../../Components/SetContainer/SetContainer';
+import classes from './Workout.module.scss';
 
 class Workout extends Component {
+  state = {
+    title: 'Lower Body 1',
+    setItems: [
+      {
+        title: 'Barbel Squat',
+        sets: [
+          { weight: 50, reps: 8, isComplete: false },
+          { weight: 50, reps: 8, isComplete: false }
+        ]
+      },
+      {
+        title: 'Leg Curl',
+        sets: [
+          { weight: 105, reps: 8, isComplete: false },
+          { weight: 105, reps: 8, isComplete: false }
+        ]
+      },
+    ],
+  }
+
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+  }
+
   render() {
     return (
       <div>
-        <h2>Lower Body 1</h2>
+        <div>
+          Start / Finish
+        </div>
+
+        <h2>{this.state.title}</h2>
+
+        <div className={classes.Sets}>
+          { this.state.setItems.map((item, index) => <div key={index}><SetContainer title={item.title} sets={item.sets} /></div>) }
+        </div>
       </div>
     );
   }
