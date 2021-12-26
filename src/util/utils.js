043014c67527696a8a827a60aedaddcad0957e84
James Hall-Treworgy <jameshall-treworgy@Jamess-MacBook-Pro.local>
2021-12-26 18:33:03 -0500

    Updated time format to handle hours. Updated history page to contain workout summary. Switched modals to fixed position.

--- src/util/utils.js
+++ src/util/utils.js
@@ -10,18 +10,34 @@ export const convertTimeFromDates = (time) => {
 }
 
 export const formatTimeFromSeconds = (t) => {
+  let oneHourInSec = 3600;
+  let oneMinInSec = 60;
+  let hour = '0';
+
+  if (t >= oneMinInSec) {
+    hour = Math.floor(t / oneHourInSec);
+    t = t - (hour * oneHourInSec);
+  }
+
+  const minutes = getRemainingTimeFromSeconds(t);
+
+  return hour + ':' + minutes
+}
+
+function getRemainingTimeFromSeconds(t) {
   if (t >= 60) {
-    const m = Math.floor(t / 60);
+    let m = Math.floor(t / 60);
     let s = t % 60;
 
     s = s < 10 ? `0${s}` : s;
+    m = m < 10 ? `0${m}` : m;
 
     return `${m}:${s}`;
   } else {
     if (t < 10) {
-      return `0:0${t}`;
+      return `00:0${t}`;
     }
 
-    return `0:${t}`;
+    return `00:${t}`;
   }
 }
